8480c7b019a1b76be408af967cd88d90ab00f8a3
mickykore <mikiyasayele6@gmail.com>
2023-04-04 22:50:13 +0300

    edit file 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
 """
+Takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response.
+"""
+import requests
 import sys
-import urllib.request
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url = sys.argv[1]
-    if __name__ == "__main__":
-        url = sys.argv[1]
-        email = sys.argv[2]
-        data = {'email': email}
-        response = requests.post(url, data=data)
-        print(response.text)
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
