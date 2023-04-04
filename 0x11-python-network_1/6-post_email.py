5a5111b8798b6c310584dbfd019c09b71438ab72
mickykore <mikiyasayele6@gmail.com>
2023-04-04 22:45:30 +0300

    edit file 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,7 +9,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    if __name__ == "__main__":
+        url = sys.argv[1]
+        email = sys.argv[2]
+        data = {'email': email}
+        response = requests.post(url, data=data)
+        print(response.text)
